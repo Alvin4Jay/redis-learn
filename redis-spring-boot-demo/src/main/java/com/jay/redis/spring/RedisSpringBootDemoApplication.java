54eaf6d78325bd95545dfba3d0b7846e85a93ff4
xuweijie <xuweijie@didi.com>
2020-02-12 23:53:18 +0800

    spring boot redis整合

---
+++ redis-spring-boot-demo/src/main/java/com/jay/redis/spring/RedisSpringBootDemoApplication.java
@@ -0,0 +1,12 @@
+package com.jay.redis.spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RedisSpringBootDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RedisSpringBootDemoApplication.class, args);
+	}
+}
---
+++ redis-spring-boot-demo/src/test/java/com/jay/redis/spring/RedisSpringBootDemoApplicationTests.java
@@ -0,0 +1,36 @@
+package com.jay.redis.spring;
+
+import com.jay.redis.spring.bean.User;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.ValueOperations;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import javax.annotation.Resource;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class RedisSpringBootDemoApplicationTests {
+
+    @Resource
+    private RedisTemplate<String, Object> redisTemplate;
+
+    @Test
+    public void test01() {
+        ValueOperations<String, Object> valueOperations = redisTemplate.opsForValue();
+        valueOperations.set("hello", "redis");
+        System.out.println("useRedisDao = " + valueOperations.get("hello"));
+    }
+
+    @Test
+    public void test02() {
+        ValueOperations<String, Object> valueOperations = redisTemplate.opsForValue();
+        User user = new User();
+        user.setName("xuan");
+        user.setPassword("123");
+        valueOperations.set("user", user);
+        System.out.println("useRedisDao = " + valueOperations.get("user"));
+    }
+}
